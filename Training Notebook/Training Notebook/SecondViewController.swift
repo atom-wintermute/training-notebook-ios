4855666dfdf154ec805e1d8483380e9d122e7bce
Nikulin Max <m.nikulin@movavi.com>
2017-11-18 22:20:05 +0700

    Create project

---
+++ Training Notebook/Training Notebook/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Training Notebook
+//
+//  Created by m.nikulin on 18/11/2017.
+//  Copyright © 2017 atom-wintermute. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ Training Notebook/Training NotebookTests/Training_NotebookTests.swift
@@ -0,0 +1,36 @@
+//
+//  Training_NotebookTests.swift
+//  Training NotebookTests
+//
+//  Created by m.nikulin on 18/11/2017.
+//  Copyright © 2017 atom-wintermute. All rights reserved.
+//
+
+import XCTest
+@testable import Training_Notebook
+
+class Training_NotebookTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
