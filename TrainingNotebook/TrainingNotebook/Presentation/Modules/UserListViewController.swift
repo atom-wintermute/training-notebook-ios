0599621ab1a56a3acc7a648d8726e7b9a27611dd
Nikulin Max <m.nikulin@movavi.com>
2017-11-18 23:46:00 +0700

    Create storyboard for 'user list' userstory

---
+++ TrainingNotebook/TrainingNotebook/Presentation/Modules/UserListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UserListViewController.swift
+//  Training Notebook
+//
+//  Created by m.nikulin on 18/11/2017.
+//  Copyright © 2017 atom-wintermute. All rights reserved.
+//
+
+import UIKit
+
+final class UserListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
